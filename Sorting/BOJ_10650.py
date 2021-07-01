2e6d882327b3d90ae7c924a764c37f2947726bbb
gocyzhod <44426450+gocyzhod@users.noreply.github.com>
2021-07-01 17:40:28 +0900

    .

---
+++ Sorting/BOJ_10650.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = [[0]*2 for _ in range(N)]
+
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    arr[i][0] = a
+    arr[i][1] = b
+
+arr = sorted(arr ,  key = lambda arr : (arr[0], arr[1]))
+
+for i in range(N):
+    print(arr[i][0], arr[i][1])
